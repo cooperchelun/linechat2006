5a71a7b2b529518e0da1cc0f19fae9b30a54f288
cooperchelun <cooperchelun@gmail.com>
2026-06-11 17:50:36 +0800

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,10 +1,5 @@
 module.exports = async (req, res) => {
-
-    console.log("🔥 WEBHOOK HIT");
-
-    console.log("METHOD:", req.method);
-
-    console.log("BODY:", JSON.stringify(req.body));
+    console.log("🔥 LINE 有打進來");
 
     return res.status(200).json({
         ok: true
