03df7baa083f75e135c5ae81b6b5153f82727ec5
cooperchelun <cooperchelun@gmail.com>
2026-06-11 17:05:11 +0800

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,5 +1,17 @@
 module.exports = async (req, res) => {
-    res.status(200).json({
-        message: "Line健指部 API 運作中"
-    });
+    if (req.method === "GET") {
+        return res.status(200).json({
+            message: "Line健指部 API 運作中"
+        });
+    }
+
+    if (req.method === "POST") {
+        const body = req.body;
+
+        console.log("LINE 傳來的訊息：", body);
+
+        return res.status(200).json({
+            status: "received"
+        });
+    }
 };
